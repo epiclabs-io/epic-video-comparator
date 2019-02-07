2bd93563d842c0feb825419d4020cbe95a4c1a9e
Adrián Caballero <adrian@epiclabs.io>
2019-02-07 09:37:33 +0100

    Exporting interface definitions

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,3 +1,5 @@
 import { Comparator } from './Comparator';
 import { IComparatorConfig } from './models';
 export declare function newComparator(config: IComparatorConfig, container: HTMLDivElement): Comparator;
+export * from './Comparator';
+export * from './models';
--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,6 @@ import { IComparatorConfig } from './models';
 export function newComparator(config: IComparatorConfig, container: HTMLDivElement) {
     return new Comparator(config, container);
 }
+
+export * from './Comparator';
+export * from './models';
